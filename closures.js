82e6492d777495b86721f9e1a20c92d3fe6dfbea
distoneman <distoneman@gmail.com>
2018-12-20 16:43:58 -0700

    finishec

--- closures.js
+++ closures.js
@@ -13,6 +13,7 @@ function outer() {
   var name = 'Tyler';
   return function() {
     return 'The original name was ' + name;
+
   };
 }
 // Do not edit the code above.
@@ -23,13 +24,14 @@ function outer() {
 */
   
 // Code Here
-
+var inner = outer();
 
 
 //Once you do that, invoke inner.
 
 //Code Here
 
+console.log(inner());
 
 
 ////////// PROBLEM 2 //////////
@@ -53,7 +55,8 @@ function callFriend(name) {
 
 //Code Here
 
-
+var callJake = callFriend('Jake');
+console.log(callJake('435-555-9248'))
 
 ////////// PROBLEM 3 //////////
 
@@ -62,15 +65,21 @@ function callFriend(name) {
 */
 
 //Code Here
-
+function makeCounter(){
+  var x = 0
+  function increment () {
+    return (x += 1)
+  }
+  return increment
+}
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,17 +96,22 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
+  var x = value;
   return {
-
+    inc: function(){
+      return x += 1
+    },
+    dec: function(){
+      return x -= 1
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +127,11 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
-
+  function message(){
+    return(`${welcomeText} ${firstname} ${lastname}.`)
+  }
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,10 +160,14 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function (){
+      return privateMethod()
+    }
+    
   };
 })();
 
-
+console.log(module.publicMethod())
 
 ////////// PROBLEM 7 //////////
 
@@ -163,6 +183,12 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function(num){
+      return (secret += num)
+    },
+    takeAwayFromSecret: function(num){
+      return (secret -= num)
+    }
   };
 }
 
@@ -187,7 +213,7 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 5; i++) {
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
